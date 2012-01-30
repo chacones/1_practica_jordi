ebd178ed67205c197db4e526273dca7573a21030
Jordi Chacon <jordi.chacon@klarna.com>
2012-01-30 23:29:23 +0100

    Implemented main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "Parking.hpp"
+#include "utils.PRO2"
+
+using namespace std;
+
+
+int main() {
+  int accion;
+  Parking p;
+  p.read_parking();
+  while(cin >> accion and tarea != -1) {
+    if(tarea == 1) p.meter_coche();
+    else if(tarea == 2) p.sacar_coche();
+    else if(tarea == 3) p.plazas_libres();
+  }
+}

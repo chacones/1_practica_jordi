29d7db88dc6ccf31e792f3bfc9ee36afd2efdf01
Jordi Chacon <jordi.chacon@klarna.com>
2012-01-30 23:21:22 +0100

    Implemented first version of Parking.cpp

---
+++ Parking.cpp
@@ -0,0 +1,36 @@
+#include "Parking.hpp"
+
+Parking::Parking() {
+}
+
+Parking::~Parking() {
+}
+
+void Parking::read_parking() {
+  cin >> plazas_libres;
+}
+
+void Parking::meter_coche() {
+  Coche c;
+  c.read_coche();
+  aparcados.push_front(c);
+  plazas_libres--;
+}
+
+void Parking::sacar_coche() {
+  boolean found = false;
+  int matricula_a_sacar;
+  cin >> matricula_a_sacar;
+  for(list<Coche>::iterator it = aparcados.begin();
+      it != aparcados.end() && !found; it++) {
+    if(it->get_matricula() == matricula_a_sacar) {
+      aparcados.erase(it);
+      found = true;
+    }
+  }
+  plazas_libres++;
+}
+
+void Parking::plazas_libres() {
+  return plazas_libres;
+}
